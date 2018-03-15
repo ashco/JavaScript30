caf681388ecde458f064a275644787cef393ca6d
Ashton <ashchristie@gmail.com>
2018-03-15 11:46:07 -0700

    completed day 13

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,9 +7,7 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
-
 // create our functions
-
 
 function togglePlay() {
   if (video.paused) {
@@ -39,16 +37,15 @@ function handleRangeUpdate() {
 }
 
 function handleProgress() {
-  const percent = (video.currentTime / video.duration) * 100;
+  const percent = video.currentTime / video.duration * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
 
 function scrub(e) {
-  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  const scrubTime = e.offsetX / progress.offsetWidth * video.duration;
   video.currentTime = scrubTime;
   // console.log(e.offsetX);
 }
-
 
 // hook up event listeners
 video.addEventListener('click', togglePlay);
@@ -57,7 +54,6 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 
-
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
@@ -65,8 +61,8 @@ ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
-progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
 // progress.addEventListener('mouseout', () => mousedown = false);
 
 progress.addEventListener('click', scrub);
