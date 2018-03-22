5cdc36c8a1099d1b9e38760d13384e2d73c21257
Ashton <ashchristie@gmail.com>
2018-03-21 23:51:09 -0700

    1 more day to go

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,7 +1,57 @@
+let countdown;
+
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
 function timer(seconds) {
+  // stop previous timer
+  clearInterval(countdown);
+
   const now = Date.now();
   // prettier-ignore
-  const then = now - (seconds * 1000);
+  const then = now + (seconds * 1000);
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
-  console.log(now, then);
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
 }
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemaining = seconds % 60;
+  const display = `${minutes}:${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
+
+  document.title = display;
+
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour % 12}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const minutes = this.minutes.value;
+  timer(minutes * 60);
+  this.reset();
+});
